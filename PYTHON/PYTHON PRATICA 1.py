cb6fb5f0c83b41aa8978e380c0c9a4ddf8395c09
IGORZCOUTO <IGORMOURA7129@GMAIL.COM>
2024-05-03 12:17:42 -0300

    Create PYTHON PRATICA 1.py
    PYTHON 1
    PYTHON
    1º
    numero1_inteiro=int(input("insira seu número float:"))
    numero2_inteiro=int(input("insira seu número float:"))
    numero3_inteiro=int(input("insira seu número float:"))
    
    print((numero1_inteiro+ numero2_inteiro+ numero3_inteiro)/3)
    
    -----------------------------------------------------------------------------------
    
    2º
    numero1_inteiro=int(input("insira seu número:"))
    numero2_inteiro=int(input("insira seu número:"))
    
    print(numero1_inteiro + numero2_inteiro)
    
    -----------------------------------------------------------------------------------
    
    3º
    idade_inteiro=int(input("insira sua idade:"))
    
    print("quantos dias você tem:",(idade_inteiro*365))
    
    -----------------------------------------------------------------------------------
    
    4º
    numero_inteiro=int(input("insira seu número: "))
    
    print(numero_inteiro * numero_inteiro)
    
    -----------------------------------------------------------------------------------
    
    5º
    numero_inteiro=int(input("insira seu número:"))
    
    if(numero_inteiro %2 ==0 ):
        print("seu numero é par")
    else:
        print("seu numero é impar")
    
    -----------------------------------------------------------------------------------
    
    6º
    numero_float=float(input('insira seus metros:'))
    
    print(numero_float *100)
    
    -----------------------------------------------------------------------------------
    
    7º
    numero1_float=float(input("quanto ganha:"))
    numero2_float=float(input("quantas horas:"))
    
    print((numero1_float * numero2_float)*3)
    
    ----------------------------------------------------------------------------
    
    8º
    numero_float=float(input("insira o diametro de um circulo:"))
    
    print(3.14 * numero_float * numero_float) 
    
    ----------------------------------------------------------------------------
    
    9º
    numero_float=float(input("insira o diametro de um quadrado:"))
    
    print(numero_float * numero_float)
    
    ----------------------------------------------------------------------------
    
    10º
    num_float=float(input("quanto você ganha:"))
    
    print(num_float % 5 )
    print(num_float % 15)
    print(num_float % 30)
    
    ----------------------------------------------------------------------------
    
    11º
    nome=input("insira seu nome:")
    apelido=input("insira seu apelido:")
    idade_inteiro=int(input("insira sua idade:"))
    
    
    print("seja bem vindo:",nome, apelido, idade_inteiro)

---
+++ PYTHON/PYTHON PRATICA 1.py
@@ -0,0 +1,88 @@
+PYTHON 1
+PYTHON
+1º
+numero1_inteiro=int(input("insira seu número float:"))
+numero2_inteiro=int(input("insira seu número float:"))
+numero3_inteiro=int(input("insira seu número float:"))
+
+print((numero1_inteiro+ numero2_inteiro+ numero3_inteiro)/3)
+
+-----------------------------------------------------------------------------------
+
+2º
+numero1_inteiro=int(input("insira seu número:"))
+numero2_inteiro=int(input("insira seu número:"))
+
+print(numero1_inteiro + numero2_inteiro)
+
+-----------------------------------------------------------------------------------
+
+3º
+idade_inteiro=int(input("insira sua idade:"))
+
+print("quantos dias você tem:",(idade_inteiro*365))
+
+-----------------------------------------------------------------------------------
+
+4º
+numero_inteiro=int(input("insira seu número: "))
+
+print(numero_inteiro * numero_inteiro)
+
+-----------------------------------------------------------------------------------
+
+5º
+numero_inteiro=int(input("insira seu número:"))
+
+if(numero_inteiro %2 ==0 ):
+    print("seu numero é par")
+else:
+    print("seu numero é impar")
+
+-----------------------------------------------------------------------------------
+
+6º
+numero_float=float(input('insira seus metros:'))
+
+print(numero_float *100)
+
+-----------------------------------------------------------------------------------
+
+7º
+numero1_float=float(input("quanto ganha:"))
+numero2_float=float(input("quantas horas:"))
+
+print((numero1_float * numero2_float)*3)
+
+----------------------------------------------------------------------------
+
+8º
+numero_float=float(input("insira o diametro de um circulo:"))
+
+print(3.14 * numero_float * numero_float) 
+
+----------------------------------------------------------------------------
+
+9º
+numero_float=float(input("insira o diametro de um quadrado:"))
+
+print(numero_float * numero_float)
+
+----------------------------------------------------------------------------
+
+10º
+num_float=float(input("quanto você ganha:"))
+
+print(num_float % 5 )
+print(num_float % 15)
+print(num_float % 30)
+
+----------------------------------------------------------------------------
+
+11º
+nome=input("insira seu nome:")
+apelido=input("insira seu apelido:")
+idade_inteiro=int(input("insira sua idade:"))
+
+
+print("seja bem vindo:",nome, apelido, idade_inteiro)
